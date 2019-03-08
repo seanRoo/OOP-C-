20d46993bdd28eb910550787d6c1991cbb81b83d
Sean Rooney <33811391+seanRoo@users.noreply.github.com>
2019-03-08 15:34:57 +0000

    Update StackOverflowPost.cs

--- StackOverflowPost.cs
+++ StackOverflowPost.cs
@@ -1,3 +1,10 @@
+/*
+Design a class called Post. This class models a StackOverflow post. It should have properties
+for title, description and the date/time it was created. We should be able to up-vote or down-vote
+a post. We should also be able to see the current vote value. In the main method, create a post,
+up-vote and down-vote it a few times and then display the the current vote value. 
+*/
+
 using System;
 
 class Program{
